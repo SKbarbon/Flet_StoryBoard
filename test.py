9509a85dcebdcbe6d6465cc0ccd11187430aa2d1
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-04-24 17:13:36 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from Flet_StoryBoard import LoadStoryBoard, StoryBoard
-import flet
-import time
-
-
-def main (storyBoard:StoryBoard):
-    pass
-
-LoadStoryBoard(target_function=main, storyboard_file_path="myUI.fletsb")

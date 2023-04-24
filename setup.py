1aaabdd4d7c7d391dd04be10d82958a2b1a84752
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-04-24 18:48:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name='Flet StoryBoard',
-    version='0.2.6',
+    version='1.0',
     author='SKbarbon',
     description='A UI-Builder that helps programmers build the front-end without codding it.',
-    long_description=long_description,
+    long_description="https://github.com/SKbarbon/Flet-StoryBoard",
     url='https://github.com/SKbarbon/Flet-StoryBoard',
     install_requires=["flet==0.5.2", "requests"],
     packages=find_packages(),

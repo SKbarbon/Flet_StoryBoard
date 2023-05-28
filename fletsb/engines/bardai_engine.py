4df3e771a49f4b829acba05adb4f53db3458a116
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-28 23:26:05 +0300

    Support bard AI as an AI builder

--- fletsb/engines/bardai_engine.py
+++ fletsb/engines/bardai_engine.py
@@ -7,9 +7,11 @@ import os, flet, random, time, json
 
 new_widgets_template = """
 You are an AI for UI builder in a software called `Flet_StoryBoard`. Users will ask you to make something on their storyboard. The storyboard is a actually a json file that contain a UI stuff. Note that all your responses must be as json syntax. If a user tell you anything that unrelated to your job, return a `ok=false` on json. You can add new widgets. If the user ask you to put a widget that is not defined down bellow, then you choose one of the available widgets bellow that are similar to it.
+Please make sure that you are making a good UI/UX practice with the widgets properties. For example if the title is on center alianment then make the paragraph alianment also in center. The page color is </bg_page/>, so make sure that you are not using the same color of the page color for the text color so the user can see the text.
 
 The widgets are available are with their own properties:
 - Title: title, title_color, size, width, italic, bold, hide, expand, alignment, text_align.
+- Paragraph: text, text_color, size, width, italic, bold, hide, alignment, text_align.
 - Button: text, function_name, text_color, bgcolor, alignment, width, height, hide.
 - Markdown: content, width, height, alignment.
 - Open Url: text, url, text_color, bgcolor, alignment, width, height, border_radius, hide.
@@ -61,6 +63,8 @@ The user message is:
 </usermessage/>
 ```
 
+Please make sure that you are making a good UI/UX practice with the widgets properties. For example if the title is on center alianment then make the paragraph alianment also in center. The page color is </bg_page/>, so make sure that you are not using the same color of the page color for the text color so the user can see the text.
+Use each widget for its correct job. For example if you want a link to open in the browser, use the `Open Url` widget.
 Dont set the `widget_class_name` on the json to a name that does not exists above, For example if the user ask for a text, there is no Text widget, so you must search for a similar supported widget like the `title`.
 Do not use any widget name except: Title, Button, Markdown, Open Url, Image or TextField.
 Put all the widgets as shown on the previous json example.
@@ -75,9 +79,9 @@ class BardapiSupport:
         container = flet.Container(on_click=self.close_the_input)
         self.container = container
 
-        if self.get_bard_token() != None:
+        if self.get_bard_token() == None:
             bard_token = flet.TextField(hint_text="Put your bard Token..", 
-                                        bgcolor="white", color="black")
+                            bgcolor="white", color="black", on_submit=self.save_the_token)
             container.content = flet.Column([
                 flet.Row([flet.Text("Your Bard Token")], alignment="center"),
                 flet.Row([bard_token], alignment="center"),
@@ -115,13 +119,13 @@ class BardapiSupport:
         e.control.update()
         
         try: result = self.ask_bard(e.control.value)
-        except: print("Error with connecting with bard."); return
+        except: print("Error with connecting with bard."); self.close_the_input(self.container); return
 
         try:
             result = self.load_the_respone_to_dict(result)
         except Exception as e:
             print(f"Error:\n{e}")
-            self.ress()
+            self.close_the_input(self.container)
             return
         
         for new_widget in result['all']:
@@ -131,12 +135,12 @@ class BardapiSupport:
             self.main_class.edit_a_widget(len(self.main_class.dict_content["pages"][self.main_class.current_page_name]["widgets"]) - 1)
             self.main_class.page.update()
 
-        self.ress()
+        self.close_the_input(self.container)
     
     def close_the_input (self, e):
-        self.ress()
         self.main_class.main_row.opacity = 1.0
         self.main_class.main_row.update()
+        self.ress()
 
     def get_bard_token (self):
         if not os.path.isfile (".bardapi"):
@@ -147,13 +151,19 @@ class BardapiSupport:
         return bardapi_token
     
     def ask_bard (self, message):
+        self.container.content = flet.Column([flet.Row([
+            flet.Text("The AI is thinking 😃!", weight="bold", size=26)
+        ], alignment="center")], alignment="center")
+        self.container.update()
+
         token = self.get_bard_token()
         if token == None:
             return False
 
         os.environ['_BARD_API_KEY'] = f"{token}"
-        bard = Bard(timeout=10)
+        bard = Bard(timeout=12)
         message_new = new_widgets_template.replace("</usermessage/>", message)
+        message_new = str(message_new).replace("</bg_page/>", str(self.main_class.page.bgcolor))
         return str(bard.get_answer(message_new)['content'])
     
     def load_the_respone_to_dict (self, respone:str):
@@ -172,4 +182,10 @@ class BardapiSupport:
     
     def how_to_get_bard_token_page (self, e):
         p : flet.Page = e.page
-        p.launch_url("")
+        p.launch_url("https://github.com/SKbarbon/Flet_StoryBoard/blob/main/docs/get_bard_token.md")
+        self.close_the_input(e)
+    
+    def save_the_token (self, token):
+        token = token.control.value
+        open(".bardapi", "w+", encoding="utf-8").write(token)
+        self.close_the_input(token)
